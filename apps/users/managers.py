4f801bbd055e1acec1b740aa242a6ff3dcf3324a
Mirael <bogdanova.karina22@gmail.com>
2024-03-24 20:01:54 +0200

    user model is created, custom user manager is created

---
+++ apps/users/managers.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import BaseUserManager
+from django.contrib.auth.hashers import make_password
+
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+    
+    def create_user(self, email, password, first_name='', last_name='', phone=''):
+        if not email:
+            raise ValueError("Email is required")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.password = make_password(password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.phone = phone
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password):
+        user = self.create_user(email=email, password=password)
+        
+        user.is_staff = True
+        user.is_email_verified = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,46 @@
+import uuid
+
 from django.db import models
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.utils.translation import gettext_lazy as _
+from django.utils import timezone
+
+from apps.users.managers import CustomUserManager
 
 
-# class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     
+    id = models.UUIDField(_('id'), primary_key=True, default=uuid.uuid4)
+    email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=150, blank=True, default='')
+    last_name = models.CharField(_("last name"), max_length=150, blank=True, default='')
+    phone = models.CharField(_("phone number"), max_length=15, blank=True, default='')
+    is_staff = models.BooleanField(
+        _("staff status"),
+        default=False,
+        help_text=_("Designates whether the user can log into this admin site."),
+    )
+    is_active = models.BooleanField(
+        _("active"),
+        default=True,
+        help_text=_(
+            "Designates whether this user should be treated as active. "
+            "Unselect this instead of deleting accounts."
+        ),
+    )
+    date_joined = models.DateTimeField(_("date joined"), default=timezone.now)
+    updated_at = models.DateTimeField(_('updated'), auto_now=True)
+    is_email_verified = models.BooleanField(_('email verified'), default=False)
+    
+    objects = CustomUserManager()
+    
+    USERNAME_FIELD = "email"
+    EMAIL_FIELD = "email"
+    
+    def __str__(self) -> str:
+        return self.email
+    
+    class Meta:
+        verbose_name = _("user")
+        verbose_name_plural = _("users")
