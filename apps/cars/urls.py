f965190cfe844e4da5fc87483bfeec27a4d72120
Mirael <bogdanova.karina22@gmail.com>
2024-03-31 00:25:34 +0200

    change car owner API is added

--- apps/cars/urls.py
+++ apps/cars/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from apps.cars.views.create_car import CreateCar
 from apps.cars.views.car_info import CarInfoRetrieveUpdateAPIView
 from apps.cars.views.car_info import CarInfoListAPIView
+from apps.cars.views.change_car_owner import CarOwnerUpdateAPIView
 
 
 app_name = 'cars'
@@ -11,4 +12,5 @@ urlpatterns = [
     path('create/', CreateCar.as_view(), name='create-car'),
     path('info/<slug:pk>', CarInfoRetrieveUpdateAPIView.as_view(), name='car-info'),
     path('list/', CarInfoListAPIView.as_view(), name='car-list'),
+    path('change-owner/<slug:pk>', CarOwnerUpdateAPIView.as_view(), name='change-car-owner'),
 ]
