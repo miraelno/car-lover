54b32623329c3c50f8907467bd120c8ffd9f84d2
Bogdan <104357137+Bogdandrummer@users.noreply.github.com>
2024-04-13 18:23:01 +0300

    user page layout done

--- frontend/js/user_page.js
+++ frontend/js/user_page.js
@@ -55,18 +55,43 @@ confItem.forEach(el => {
 });
 
 //Add photo
+
+
 const photoRow = document.querySelector('.photo__row');
 const addImg = document.getElementById('add__btn');
 
+
 const download = (input) => {
+	
 	let file = input.files[0];
 	let reader = new FileReader();
 	reader.readAsDataURL(file);
 
 	reader.onload = function () {
-		let img = document.createElement('img');
-		addImg.before(img);
-		img.src = reader.result;
-		img.classList.add('.img__card');
+		// let img = document.createElement('img');
+		// addImg.before(img);
+		// img.src = reader.result;
+		// img.classList.add('.img__card');
+		const cardTemplate = document.querySelector('[data-card_template]');
+		console.log(cardTemplate);	
+		const newCard = document.importNode(cardTemplate.content, true);
+		// const fragment = document.createDocumentFragment();
+		
+		// fragment.append(newCard);
+		addImg.before(newCard);
+		newCard.querySelector('[data-image]').src = reader.result;
+	return newCard;	
 	}
 }
+
+//Notifications
+
+const bellBtn = document.getElementById('bell__btn');
+const notList = document.querySelector('.notifications__list');
+
+const showNotSet = () => {
+notList.classList.toggle('hidden');
+}
+
+bellBtn.addEventListener('click', showNotSet);
+notList.addEventListener('mouseleave', showNotSet);
