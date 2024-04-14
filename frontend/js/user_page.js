b965e836994fb175dfbae3ca6645de0ccfc8ae6a
Bogdan <104357137+Bogdandrummer@users.noreply.github.com>
2024-04-15 00:14:16 +0300

    Stages page almost finished

--- frontend/js/user_page.js
+++ frontend/js/user_page.js
@@ -1,20 +1,3 @@
-//Burger menu
-const burgerBtn = document.getElementById('burger__btn');
-const headerMenu = document.querySelector('.header__menu');
-
-const showMenu = () => {
-headerMenu.classList.toggle('d__flex');
-}
-
-const hideMenu = () => {
-	if (window.innerWidth > '997') {
-		headerMenu.classList.remove('d__flex');
-	}
-}
-
-window.addEventListener('resize', hideMenu);
-burgerBtn.addEventListener('click', showMenu);
-
 //Edit form fields
 const nameInp = document.getElementById('user__name-info');
 const emailInp = document.getElementById('user__email.info');
